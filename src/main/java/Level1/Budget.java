3a95a4f602727d50a4327fa239775d2463fc3f9b
dudeoeoeo <dudekekek@naver.com>
2021-10-09 00:12:56 +0900

    DFS prac

--- src/main/java/Level1/Budget.java
+++ src/main/java/Level1/Budget.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 public class Budget {
     public int solution(int[] d, int budget) {
         int answer = 0;
+
         Arrays.sort(d);
+
         for(int bg : d) {
             budget -= bg;
-            if(budget >= 0)
-                answer++;
-            else
+
+            if(budget < 0)
                 break;
+
+            answer++;
         }
         return answer;
     }
